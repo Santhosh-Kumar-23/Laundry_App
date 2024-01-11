0bd7e3ce48914d4e6f849bc25b0f3b4602920f31
Santhosh-Kumar-23 <santhoshmoorthycolanonline@gmail.com>
2024-01-11 15:04:58 +0530

    created cardview component(10%)

---
+++ src/components/CardView/styles.tsx
@@ -0,0 +1,23 @@
+import {StyleSheet, Dimensions} from 'react-native';
+
+const styles = StyleSheet.create({
+  cardViewContainer: {
+    backgroundColor: 'yellow',
+    height: 240,
+    marginHorizontal: 20,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  textContainer: {
+    backgroundColor: 'pink',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginHorizontal: 22,
+    marginVertical: 4,
+  },
+  textStyle: {
+    fontWeight: '700',
+    color: 'black',
+  },
+});
+export default styles;
--- src/utils/types.tsx
+++ src/utils/types.tsx
@@ -1,4 +1,12 @@
-
+import {
+ StyleProp,
+ ViewStyle,
+} from 'react-native';
   import type { NativeStackScreenProps } from "@react-navigation/native-stack"
 
   export type ScreenProps = NativeStackScreenProps<{navigation:any}>;
+  export interface CardViewProps {
+    containerStyle?: null | StyleProp<ViewStyle>;
+    data?: Array<Object>;
+   }
+  
